f078687640a9eeb2f7eabf98e89126b366aceba7
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2026-09-25 15:51:19 -0300

    Fazendo atualizações de cores e telas

---
+++ app/src/main/java/com/example/sistemaeleicaop1/Login.kt
@@ -0,0 +1,67 @@
+package com.example.sistemaeleicaop1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    private lateinit var etNome : EditText
+
+    private lateinit var etSenha : EditText
+
+    private lateinit var btnEntrar : Button
+
+    private lateinit var btnSair : Button
+
+    private lateinit var tvStatus : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        etNome = findViewById(R.id.etNome)
+        etSenha = findViewById(R.id.etSenha)
+
+        btnEntrar = findViewById(R.id.btnEntrar)
+        btnSair = findViewById(R.id.btnSair)
+
+        tvStatus = findViewById(R.id.tvStatus)
+
+        btnEntrar.setOnClickListener {
+            var nome: String
+            var senha: String
+
+            nome = etNome.text.toString()
+            senha = etSenha.text.toString()
+
+            if(nome == "Admin" && senha == "1234"){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else if(nome == "Entrevistador" && senha == "1234"){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                tvStatus.text = "Login inválido"
+            }
+        }
+
+        btnSair.setOnClickListener {
+            finishAffinity()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/sistemaeleicaop1/MainActivity.kt
+++ app/src/main/java/com/example/sistemaeleicaop1/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.sistemaeleicaop1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnEleitores : Button
+    private lateinit var btnResultado : Button
+    private lateinit var btnLimparDados : Button
+    private lateinit var btnSair : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnEleitores = findViewById(R.id.btnEleitores)
+        btnResultado = findViewById(R.id.btnResultado)
+        btnLimparDados = findViewById(R.id.btnLimparDados)
+        btnSair = findViewById(R.id.btnSair)
+
+        btnEleitores.setOnClickListener {
+            val intent = Intent(this, Eleitores::class.java)
+            startActivity(intent)
+        }
+
+        btnResultado.setOnClickListener {
+            val intent = Intent(this, Resultado::class.java)
+            startActivity(intent)
+        }
+
+        btnLimparDados.setOnClickListener {
+
+        }
+
+        btnSair.setOnClickListener {
+            finishAffinity()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
